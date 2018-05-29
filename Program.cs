f253ccc41d1be16ca6f9255959767b5a323d3803
Mark McGookin <mark.mcgookin@gmail.com>
2018-05-29 12:05:27 +0100

    Initial commit
    Initial commit

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace example
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+            .ConfigureAppConfiguration((context, config) =>
+                {
+                    var currentDir = Directory.GetCurrentDirectory();
+                    config.SetBasePath(currentDir)
+                        .AddJsonFile("appsettings.json", optional: false)
+                        .AddEnvironmentVariables();
+
+                    var builtConfig = config.Build();
+                    config.AddAzureKeyVault(
+                        $"https://{builtConfig["KeyVaultName"]}.vault.azure.net/",
+                        builtConfig["ClientId"],
+                        builtConfig["ClientSecret"]);
+                })
+                .UseStartup<Startup>()
+                .Build();
+    }
+}
